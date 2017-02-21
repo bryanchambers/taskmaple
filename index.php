222458ca5815c7a2942f2aadf62cde0cec13b68f
Bryan <bryches@BrychesT420.Ubuntu>
2017-02-21 00:07:12 -0500

    Added Bootstrap

--- index.php
+++ index.php
@@ -79,16 +79,25 @@ echo "</table>";
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-	<form method='post'>
-		<input name='position' type='number' placeholder='tree left/right value'>
-		<input name='text' type='text' placeholder='new item'>
-		<input type='submit' name='new' value='Insert New Item'><br>
+	<div class='container text-left'>
+		<div class='row'><div class='col-md-4 col-md-offset-4'>
+			<form method='post'>
+				<div class='row form-group'>
+					<div class='col-md-5'><input class='form-control' name='position' type='number' placeholder='tree value'></div>
+					<div class='col-md-5'><input class='form-control' name='text' type='text' placeholder='text'></div>
+					<div class='col-md-2'><input class='btn btn-default' type='submit' name='new' value='Add Item'></div>
+				</div>
 
-		<input name='left' type='number' placeholder='tree left value'>
-		<input name='right' type='text' placeholder='tree right value'>
-		<input type='submit' name='delete' value='Delete Item'>
-	</form>
+				<div class='row form-group'>
+					<div class='col-md-5'><input class='form-control' name='left' type='number' placeholder='tree left'></div>
+					<div class='col-md-5'><input class='form-control' name='right' type='text' placeholder='tree right'></div>
+					<div class='col-md-2'><input class='btn btn-default' type='submit' name='delete' value='Delete Item'></div>
+				</div>
+			</form>
+		</div></div>
+	</div>
 </body>
 </html>
